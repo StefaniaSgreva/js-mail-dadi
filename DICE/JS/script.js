81b7f676b6a961e9697e0d0ba0477bb439eda924
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-11 17:22:28 +0200

    added random image for the first dice

--- DICE/JS/script.js
+++ DICE/JS/script.js
@@ -5,9 +5,9 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-const randomNumber = [];
-const diceNumbers = 6;
 
-for(let i = 1; i <= 6; i++){
-    let diceNumber = Math.floor(Math.random() *  (6 - 1 + 1) ) + 1;
-}
+const firstRandomNum = Math.floor(Math.random() *  (6 - 1 + 1) ) + 1;
+
+//img/dice-append firstRandomNu - + png ( from 1 to 6)
+const firstDiceImg = './img/dice' + firstRandomNum + '.png';
+document.querySelectorAll('img')[0].setAttribute('src', firstDiceImg);
